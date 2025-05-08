074f6a15a933493fa5346848271b2fe604d6f11f
MaguguEl <maguguelvis@gmail.com>
2025-05-08 15:29:28 +0200

    easy does it.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const MainNavigation = () => {
   return (
     <nav className="border-bottom border-secondary-subtle">
-      <div className="container-xl d-flex justify-content-center align-items-center py-3">
-        <div className="nav gap-4 fw-semibold text-secondary">
-          <Link to="/" className="text-decoration-none text-secondary">Home</Link>
-          <Link to="/shop" className="text-decoration-none text-secondary">Shop</Link>
-          <Link to="/categories" className="text-decoration-none text-secondary">Top Categories</Link>
-          <Link to="/deals" className="text-decoration-none text-secondary">Deals</Link>
-          <Link to="/affliations" className="text-decoration-none text-secondary">Affliates aand Creators</Link>
+      <div className="container-xl d-flex justify-content-between align-items-center py-3">
+        <div className="d-flex align-items-center gap-4">
+          <div className="nav nav-bottom justify-content-start gap-4 fw-semibold text-secondary">
+            <Link to="/" className="text-decoration-none text-secondary">Home</Link>
+            <Link to="/shop" className="text-decoration-none text-secondary">Shop</Link>
+            <Link to="/categories" className="text-decoration-none text-secondary">Top Categories</Link>
+            <Link to="/deals" className="text-decoration-none text-secondary">Deals</Link>
+            <Link to="/affliations" className="text-decoration-none text-secondary">Affliates</Link>
+          </div>
+        </div>
+        <div className="d-flex gap-3 text-secondary">
+          <Link to="#" className="d-flex align-items-center gap-1 text-decoration-none text-secondary">
+              <FontAwesomeIcon icon="user" size="lg" /> Sign In
+          </Link>
         </div>
       </div>
     </nav>
